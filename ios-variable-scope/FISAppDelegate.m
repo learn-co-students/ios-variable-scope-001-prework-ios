ea15539e3da4b490c39936d4f3e55e74dcf43805
richardgmartin <richardgeorgemartin@gmail.com>
2015-11-05 15:45:04 -0500

    completed ios scope exercise

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,42 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+// logic for arrayAddingString:toArray method
+
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    
+    // create copy of incoming array
+    
+    NSMutableArray *arrayCopy = [array mutableCopy];
+    
+    [arrayCopy addObject:string];
+    
+    return arrayCopy;
+    
+}
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    
+    NSUInteger uppcaseStringCount = 0;
+    
+    NSMutableArray *arrayAgain = [array mutableCopy];
+    
+    NSUInteger arrayCount = [arrayAgain count];
+    
+    for (NSUInteger i = 0; i < arrayCount; i++) {
+        NSString *stringCopy = [arrayAgain[i] uppercaseString];
+        
+        if ([stringCopy isEqualToString:arrayAgain[i]]) {
+            uppcaseStringCount++;
+        }
+    }
+    
+    return uppcaseStringCount;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array {
+    
+    [array removeAllObjects];
+}
 
 @end
