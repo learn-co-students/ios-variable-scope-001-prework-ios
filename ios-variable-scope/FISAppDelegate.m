6a41021deea3196c11a90c194795906f03e5ebe2
Shena Yoshida <sheyoshida@gmail.com>
2015-12-15 12:11:34 -0500

    Done.

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,33 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    
+    NSMutableArray *newArray = [array mutableCopy];
+    NSString *newString = string;
+    [newArray addObject:newString];
+    
+    return newArray;
+}
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    
+    NSMutableArray *newArray = [array mutableCopy];
+    NSUInteger uppercaseStringCount = 0;
+    NSUInteger arrayCount = [newArray count];
+    
+    for (NSUInteger i = 0; i < arrayCount; i++) {
+        NSString *stringCopy = [newArray[i] uppercaseString];
+        
+        if ([stringCopy isEqualToString:newArray[i]]) {
+            uppercaseStringCount++;
+        }
+    }
+    return uppercaseStringCount;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array {
+    [array removeAllObjects];
+}
 
 @end
