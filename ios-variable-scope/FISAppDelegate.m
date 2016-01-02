7eb3e2e46f7fad88540bc91825df80b769c002f5
shibamo <qinchao@hotmail.com>
2016-01-02 13:38:51 -0500

    pass the test

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,24 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+- (NSMutableArray *) arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array{
+    NSMutableArray * _array = [array mutableCopy];
+    [_array addObject:string];
+    return _array;
+}
+
+- (NSUInteger) countOfUppercaseStringsInArray:(NSArray *)array{
+    NSUInteger _count = 0;
+    for (NSUInteger i=0; i<[array count]; i++) {
+        if ([array[i] isEqualToString:[array[i] uppercaseString] ]) {
+            _count++;
+        }
+    }
+    return _count;
+}
+
+- (void) removeAllObjectsFromArray:(NSMutableArray *)array{
+    [array removeAllObjects];
+}
 
 @end
