9f9534ba8d97c1a84f9bf70e27e5ecba6a92fac5
eoinwhitney <whitneyeoin@gmail.com>
2016-01-13 13:29:25 -0500

    ios variable scope

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,6 +19,35 @@
     return YES;
 }
 
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array;{
+    NSMutableArray *returnedArray= [array mutableCopy];
+    [returnedArray addObject:(string)];
+    return returnedArray;
+}
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array; {
+    NSMutableArray *upperCase= [[NSMutableArray alloc] init];
+    
+    for(NSUInteger i=0; i<array.count; i++) {
+        NSString *originalWord= array[i];
+        NSString *upperCaseWord= [originalWord uppercaseString];
+        if([originalWord isEqualToString:upperCaseWord ]) {
+            [upperCase addObject:upperCaseWord];
+        }
+        
+    }
+    NSUInteger totalCount= upperCase.count;
+    return totalCount;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array; {
+    [array removeAllObjects];
+}
+    
+
+
+
+
 /*
  
  * Write your method definitions here.
