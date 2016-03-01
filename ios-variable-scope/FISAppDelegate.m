807e74219eece91d28c265278af51224929ce4a5
Dougly <dgalante11@gmail.com>
2016-03-01 13:23:22 -0500

    local tests pass

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -16,13 +16,44 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+- (NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    
+    NSMutableArray *newArray = [[NSMutableArray alloc]init];
+    for (NSUInteger i = 0; i < [array count]; i++) {
+        NSString *newString = array[i];
+        [newArray addObject:newString];
+    }
+    [newArray addObject:string];
+    return newArray;
+}
+
+- (NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    NSUInteger numberOfUppercaseStrings = 0;
+    //NSArray *punctuations = @[@".", @",", @"!", @"?", @":", @";", @" "];
+    NSUInteger arrayLength = [array count];
+    for (NSUInteger i = 0; i < arrayLength; i++) {
+        NSString *stringToCheck = array[i];
+        NSString *upperCaseString = [array[i] uppercaseString];
+        BOOL stringIsUpperCase = [stringToCheck isEqualToString:upperCaseString];
+        if (stringIsUpperCase) {
+            numberOfUppercaseStrings++;
+        }
+    }
+    return numberOfUppercaseStrings;
+}
+
+- (void)removeAllObjectsFromArray:(NSMutableArray *)array {
+    for (NSUInteger i = 0; i < [array count]; i++) {
+        [array removeObjectAtIndex:i];
+        if ([array count] == 1) {
+            [array removeObjectAtIndex:0];
+        }
+    }
+    
+}
 
 @end
