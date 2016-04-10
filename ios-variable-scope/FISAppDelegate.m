bf4b0def21d6c512b734fdba9b13d245b3a6e745
github username <github email address>
2016-04-09 22:43:28 -0400

    the lab is complete

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,30 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+//description of the method that adds a string to an array without changing the original array
+- (NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    NSMutableArray *returnArray = [[NSMutableArray alloc] init];
+    [returnArray addObjectsFromArray:array];
+    [returnArray addObject:string];
+    return returnArray;
+}
+
+
+//description of the method that counts the uppercased strings in an array
+- (NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    NSUInteger count = 0;
+    for (NSUInteger i =0; i < [array count]; i++) {
+        if ([array[i] isEqualToString:[array[i] uppercaseString]]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+//description of the method that removes all the objects from an array
+- (void)removeAllObjectsFromArray:(NSMutableArray *)array {
+    [array removeAllObjects];
+}
 
 @end
