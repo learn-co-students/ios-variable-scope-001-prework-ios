602ea1460ad067cbda422c2d662b0843efbf0f66
dpreli <dave.preli@gmail.com>
2016-04-11 14:34:31 -0400

    Done.

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,52 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+
+
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    
+    NSMutableArray *arrayCopy = [NSMutableArray arrayWithArray:array];
+    [arrayCopy addObject:string];
+    return arrayCopy;
+}
+
+-(NSUInteger)countOfStringsInAllCapsInArray:(NSArray *)array
+{
+    NSUInteger allCapCount = 0;
+    for(NSString *string in array) {
+        
+        NSString *capitalizedString = [string uppercaseString];
+        if([string isEqualToString:capitalizedString]) {
+            allCapCount++;
+        }
+    }
+    
+    return allCapCount;
+}
+
+-(void)removeAllElementsFromArray:(NSMutableArray *)array
+{
+    [array removeAllObjects];
+}
+
+
+- (NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    NSMutableArray *uppercaseStrings = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < array.count; i++) {
+        NSString *currentString = array[i];
+        NSString *upperString = [currentString uppercaseString];
+        
+        if ([currentString isEqualToString:upperString]) {
+            [uppercaseStrings addObject:currentString];
+        }
+    }
+    
+    return uppercaseStrings.count;
+}
+
+- (void)removeAllObjectsFromArray:(NSMutableArray *)array {
+    [array removeAllObjects];
+}
 
 @end
