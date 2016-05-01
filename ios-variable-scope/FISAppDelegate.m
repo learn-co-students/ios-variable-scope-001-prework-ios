a18692529c3475629642d47b93a37f1a1aa13d15
bsoung <bsoung@gm.slc.edu>
2016-04-30 22:52:42 -0400

    first commit

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,36 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
+    
+    NSMutableArray *newArray = [array mutableCopy];
+    [newArray addObject: string];
+    
+    return newArray;
+}
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    
+    NSMutableArray *upperCaseArray = [NSMutableArray array];
+    NSUInteger upperCaseCount = 0;
+    NSUInteger i = 0;
+    
+    while (i < [array count]) {
+        i = i + 1;
+        NSString *charString = array[i - 1];
+        if ([array containsObject:[charString capitalizedString]] || [array containsObject:[charString uppercaseString]]) {
+            [upperCaseArray addObject:charString];
+            upperCaseCount = [upperCaseArray count];
+            
+        }
+    }
+    
+    return upperCaseCount;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array {
+    
+    [array removeAllObjects];
+}
 
 @end
