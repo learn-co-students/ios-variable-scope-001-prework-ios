30262ea1c5b29fc7539732878933980086e3bb56
Felicity <felicity.johnson.mail@gmail.com>
2016-07-02 16:47:50 -0400

    variable-Scope

--- ios-variable-scope/FISAppDelegate.h
+++ ios-variable-scope/FISAppDelegate.h
@@ -12,9 +12,11 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
--(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array;
+-(NSArray *)arrayByAddingString:(NSString *)string toArray:(NSArray *)array;
 
 -(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array;
+
+-(NSUInteger)countOfStringsInAllCapsInArray:(NSArray *)array;
 
 -(void)removeAllObjectsFromArray:(NSMutableArray *)array;
 
--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,60 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+-(NSArray *)arrayByAddingString:(NSString *)string toArray:(NSArray *)array {
+
+    NSMutableArray *newArray = [NSMutableArray arrayWithArray:array];
+    
+    [newArray addObject:string];
+    return newArray;
+}
+
+-(NSUInteger)countOfStringsInAllCapsInArray:(NSArray *)array {
+
+    NSUInteger capCount = 0;
+    BOOL isUpperCase;
+    NSString *arrayItem = @"";
+    
+    for (NSUInteger i = 0; i < [array count]; i++) {
+        
+        arrayItem = array[i];
+        isUpperCase= [[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[arrayItem characterAtIndex:i]];
+        
+        if (isUpperCase) {
+         
+            capCount++;
+        }
+    }
+    
+    return capCount;
+}
+
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
+    
+    NSMutableArray *upperCase = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [array count]; i++) {
+        
+        NSString *arrayItem = array[i];
+        NSString *upperCaseArrayItem = [arrayItem uppercaseString];
+        
+        if ([arrayItem isEqualToString:upperCaseArrayItem]) {
+            
+            [upperCase addObject: upperCaseArrayItem];
+        }
+        
+    }
+    
+    return upperCase.count;
+}
+
+
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array{
+    
+    [array removeAllObjects];
+    
+}
 
 @end
