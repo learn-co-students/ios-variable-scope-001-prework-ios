51a15a705070391507adc9e636a1dd84ac7d089f
Paul Tangen <ptangen@ptangen.com>
2016-08-11 11:31:45 -0400

    complete the practice with scope assignment

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -16,13 +16,50 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    NSArray *arrayInit = @[ @"Joe", @"TIM", @"Jim", @"TOM", @"Mark" ];
+    NSMutableArray *array = [arrayInit mutableCopy];
+    NSString *newString = @"Jake";
+    NSArray *arrayWithNewString = [[NSMutableArray alloc] init];
+    NSUInteger upperCaseStringCount = 0;
+    
+    // test the method
+    arrayWithNewString = [self arrayByAddingString:newString toArray:array];
+    NSLog(@"arrayWithNewString: %@ , %@", newString, arrayWithNewString);
+    
+    
+    upperCaseStringCount = [self countOfUppercaseStringsInArray:array];
+    NSLog(@"upperCaseStringCount: %li", upperCaseStringCount);
+    
+    [self removeAllObjectsFromArray:array];
+    NSLog(@"removeAllObjectsFromArray: %@", array);
+    
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+
+
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array{
+    NSMutableArray *array2 = [array mutableCopy];
+    [array2 addObject:string];
+    return array2;
+}
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array{
+    NSUInteger count = 0;
+    
+    // evaluate each element in the array and count the upper case strings
+    for(NSUInteger i = 0; i < [array count]; i++){
+        NSString *currentString = array[i];
+        if([currentString.uppercaseString isEqualToString:currentString]){
+            count++;
+        }
+    }
+    return count;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array{
+        [array removeAllObjects];
+    }
 
 @end
